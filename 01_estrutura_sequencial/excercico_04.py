af3e383189a7d2ca1d9af315ac2b5508ef51e6ff
Daniela Verçosa <djccv@discente.ifpe.edu.br>
2026-08-27 16:03:18 -0300

    Implement temperature conversion from Fahrenheit to Celsius
    Add a program to convert Fahrenheit to Celsius.

--- 01_estrutura_sequencial/excercico_04.py
+++ 01_estrutura_sequencial/excercico_04.py
@@ -1,1 +1,5 @@
+#Faça um programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
 
+f = float(input('Digite a temperatura em fahrenheit: '))
+c = 5 * ((f - 32) / 9)
+print(f'{f} em celsius é: {c}')
