e4c05b457df86b6c5bb4395902aabcc47f21b3d3
Daniela Verçosa <djccv@discente.ifpe.edu.br>
2026-08-27 16:01:52 -0300

    Create exercicio_03.py

---
+++ 01_estrutura_sequencial/exercicio_03.py
@@ -0,0 +1,4 @@
+#Faça um programa que peça o raio de um círculo, calcule e mostre sua área:
+raio = float(input('Entre com o raio: '))
+área = (raio ** 2) * 3.14
+print(f'A área de círculo com raio {raio} é: {área}')
