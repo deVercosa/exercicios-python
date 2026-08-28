3bae0b0cc435ea679f52f994ee9600471f29c9b4
Daniela Verçosa <djccv@discente.ifpe.edu.br>
2026-08-28 20:45:13 -0300

    Implement bonus calculation for employee salary
    Added a program to calculate employee bonus based on salary and percentage.

--- 01_estrutura_sequencial/exercício_05.py
+++ 01_estrutura_sequencial/exercício_05.py
@@ -1,1 +1,12 @@
 
+#Escreva um programa que leia o salário de um funcionário e o
+#percentual de bônus (por exemplo, 10 para 10%), e apresente o
+#valor do bônus e o salário com o bônus somado.
+
+#Teste com salário 3200,00 e bônus de 10%.
+
+salario=float(input('salario: R$'))
+bonus= salario / 100
+print(f'o bonus mensal foi {bonus}')
+print(f' o salário mensal com bônus foi {salario + bonus}')
+
