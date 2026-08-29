4509b344b34f5c9ef136e7e94c9932ff50971f7e
Daniela Verçosa <djccv@discente.ifpe.edu.br>
2026-08-29 08:59:09 -0300

    Update exercício_07.py

--- 01_estrutura_sequencial/exercício_07.py
+++ 01_estrutura_sequencial/exercício_07.py
@@ -1,1 +1,11 @@
 
+Escreva um programa que leia uma temperatura em graus Celsius e
+apresente o valor correspondente em Fahrenheit. A fórmula é:
+
+   F = C * 9/5 + 32
+
+Teste com 28 graus Celsius.
+
+celsius = float(input("Digite a temperatura em Celsius: "))
+fahrenheit = celsius * 9 / 5 + 32
+print(f"A temperatura em Fahrenheit é: {fahrenheit}°F")
