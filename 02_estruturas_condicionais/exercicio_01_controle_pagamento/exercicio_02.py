966f148c4dd1c3a3cb1d4299a2bb90eed5f40464
Daniela Verçosa <djccv@discente.ifpe.edu.br>
2026-09-03 15:47:35 -0300

    Add number comparison program
    Implement a program to compare two numbers and print the larger one.

---
+++ 02_estruturas_condicionais/exercicio_01_controle_pagamento/exercicio_02.py
@@ -0,0 +1,10 @@
+#Faça um programa que peça dois números e imprima o maior deles
+
+numero_1=int(input('Insira o primeiro número:'))
+numero_2=int(input('Insira o primeiro número:'))
+
+if numero_1 > numero_2:
+  print(f' {numero_1} é o maior')
+  
+elif numero_1 < numero_2:
+  print(f'{numero_2} é o maior')
