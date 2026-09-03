ae1d5ce0d10010bc5d204ca7ad7805f99ddb6ee5
Daniela Verçosa <djccv@discente.ifpe.edu.br>
2026-09-03 15:54:44 -0300

    Add number positivity check program
    Implement program to check if a number is positive, negative, or zero.

--- 02_estruturas_condicionais/exercicio_01_controle_pagamento/exercicio_03.py
+++ 02_estruturas_condicionais/exercicio_01_controle_pagamento/exercicio_03.py
@@ -1,1 +1,12 @@
+#Faça um programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+ 
+numero = int(input('Insira um número: '))
 
+if numero > 0:
+    print(f'{numero} é positivo')
+
+elif numero < 0:
+    print(f'{numero} é negativo')
+    
+else:
+    print('O número é zero')
