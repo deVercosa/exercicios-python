26dc7434fb88495abbef8fb3284aeb210575cfcd
Daniela Verçosa <djccv@discente.ifpe.edu.br>
2026-09-03 16:02:07 -0300

    Add gender verification program

--- 02_estruturas_condicionais/exercicio_01_controle_pagamento/exercicio_04.py
+++ 02_estruturas_condicionais/exercicio_01_controle_pagamento/exercicio_04.py
@@ -1,1 +1,16 @@
+#Faça um programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever:
 
+F - Feminino
+M - Masculino
+Sexo Inválido.
+
+letra = input('Insira F ou M: ')
+
+if letra == 'F':
+    print('F - Feminino')
+
+elif letra == 'M':
+    print('M - Masculino')
+
+else:
+    print('Sexo Inválido')
