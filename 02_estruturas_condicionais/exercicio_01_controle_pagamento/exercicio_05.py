d1c77710629a3bd113516786befed48e664af1a7
Daniela Verçosa <djccv@discente.ifpe.edu.br>
2026-09-04 14:42:56 -0300

    Add grading system based on score input
    Implement grading system based on input score.

---
+++ 02_estruturas_condicionais/exercicio_01_controle_pagamento/exercicio_05.py
@@ -0,0 +1,14 @@
+#teste de mesa com notas
+
+nota=float(input('nota:'))
+if nota >=9:
+   print('conceito A') 
+
+elif nota >=7:
+  print('conceito B') 
+
+elif nota >=5:
+    print('conceito C') 
+
+else:
+    print('conceito D') 
